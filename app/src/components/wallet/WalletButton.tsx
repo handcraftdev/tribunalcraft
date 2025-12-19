21c150ea546dd7d6d455ef7399d8a152c514e381
onlyabrak <mhafizms@gmail.com>
2025-12-20 05:54:45 +0800

    Remove wallet provider icons from wallet button

--- app/src/components/wallet/WalletButton.tsx
+++ app/src/components/wallet/WalletButton.tsx
@@ -3,7 +3,5 @@
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 export const WalletButtonInner = () => {
-  return (
-    <WalletMultiButton className="!bg-purple-600 hover:!bg-purple-700 !rounded-lg" />
-  );
+  return <WalletMultiButton />;
 };
